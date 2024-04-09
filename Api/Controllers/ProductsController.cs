911fedc481d9ea0b20d899b6f6ec139b45e710d1
amirdotnetdev <amirkamyabi4@gmail.com>
2024-04-09 12:15:59 +0330

    commit

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "Products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Product";
+        }
+    }
+}
---
+++ Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
